01fdb8c5211dfd9202af019ff3bf7e98a0a40582
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-11-23 14:51:29 +0900

    feat: Comment entity 추가

---
+++ src/main/java/com/noah/matdongsan/model/Comment.java
@@ -0,0 +1,15 @@
+package com.noah.matdongsan.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Comment {
+    @Id
+    private Long id;
+    @Lob
+    private String comment;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created_at;
+}
