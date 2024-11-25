6d44e4309a71748fbd1e9760a063131986b5dc7b
mingyeongwon <wonmingy@gmail.com>
2024-11-25 20:37:20 +0900

    feat: QuestionAnswer ENTITY 생성

---
+++ src/main/java/com/noah/matdongsan/model/QuestionAnswer.java
@@ -0,0 +1,41 @@
+package com.noah.matdongsan.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "question_answer")
+public class QuestionAnswer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "answer_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @OneToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @Builder
+    public QuestionAnswer(String content, Question question) {
+        this.content = content;
+        this.question = question;
+    }
+}
