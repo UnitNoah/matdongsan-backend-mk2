c935ee06022402fc436c3a9e329d91eda7f26259
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-11-25 16:12:02 +0900

    feat: Amenity enum 추가

---
+++ src/main/java/com/noah/matdongsan/model/Amenity.java
@@ -0,0 +1,7 @@
+package com.noah.matdongsan.model;
+
+public enum Amenity {
+    BATH, BED, PARKING_LOT, HEATING, COOLING,
+    MICROWAVE, FRIDGE, TV, INTERNET, CLOSET,
+    DINING_TABLE, INDUCTION, WASHER
+}
