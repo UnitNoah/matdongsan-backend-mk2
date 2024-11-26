b9de4980de674a2a90161ac222590789a0427533
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-11-26 15:04:20 +0900

    feat: report 엔티티 추가

---
+++ src/main/java/com/noah/matdongsan/entity/property/Report.java
@@ -0,0 +1,26 @@
+package com.noah.matdongsan.entity.property;
+
+import com.noah.matdongsan.entity.user.CommonUser;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Lob
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+
+    @ManyToOne
+    @JoinColumn(name = "common_user_id")
+    private CommonUser commonUser;
+}
