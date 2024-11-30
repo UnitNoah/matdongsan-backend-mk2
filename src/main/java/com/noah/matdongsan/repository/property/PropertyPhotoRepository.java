4ca2ad7d9700dba99ce06cbcb053bccc66a7bfd5
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-12-01 00:01:54 +0900

    feat: 매물 id 별로 이미지들 가져오도록 PropertyPhoto에 findByProperty 메소드 추가

--- src/main/java/com/noah/matdongsan/repository/property/PropertyPhotoRepository.java
+++ src/main/java/com/noah/matdongsan/repository/property/PropertyPhotoRepository.java
@@ -4,6 +4,9 @@ import com.noah.matdongsan.entity.property.PropertyPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PropertyPhotoRepository extends JpaRepository<PropertyPhoto,Long> {
+    List<PropertyPhoto> findByProperty_id(Long propertyId);
 }
