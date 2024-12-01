1d36fbf7e570f565d2ceade5269bb761137c7fad
mingyeongwon <wonmingy@gmail.com>
2024-12-01 11:52:49 +0900

    chore: repostRepository name 변경

--- src/main/java/com/noah/matdongsan/repository/property/ReportRepository.java
+++ src/main/java/com/noah/matdongsan/repository/property/ReportRepository.java
@@ -1,8 +1,9 @@
 package com.noah.matdongsan.repository.property;
 
+import com.noah.matdongsan.entity.property.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Report extends JpaRepository<Report,Long> {
+public interface ReportRepository extends JpaRepository<Report,Long> {
 }
