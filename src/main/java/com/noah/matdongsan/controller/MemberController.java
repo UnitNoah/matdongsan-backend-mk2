19821a7f9a32c8a23845eb42d111101f4c51e87d
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-12-04 16:53:10 +0900

    feat: MemberController 생성
    - Login, 회원가입 부분 추가

---
+++ src/main/java/com/noah/matdongsan/controller/MemberController.java
@@ -0,0 +1,30 @@
+package com.noah.matdongsan.controller;
+
+import com.noah.matdongsan.dto.member.CommonUserCreateDto;
+import com.noah.matdongsan.dto.member.LoginRequestDto;
+import com.noah.matdongsan.service.member.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<String> createMember(@RequestBody @Valid CommonUserCreateDto dto) {
+        memberService.createMember(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Member created successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequest) {
+        String token = memberService.login(loginRequest.getEmail(), loginRequest.getPassword());
+        return ResponseEntity.ok(token);
+    }
+
+}
