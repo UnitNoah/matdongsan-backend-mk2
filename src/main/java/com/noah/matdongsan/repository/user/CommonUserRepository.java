39721f84c5d9d57e9d2ea975ba136d2e41ec179d
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-12-04 16:51:40 +0900

    feat: findByEmail 메소드 추가

--- src/main/java/com/noah/matdongsan/repository/user/CommonUserRepository.java
+++ src/main/java/com/noah/matdongsan/repository/user/CommonUserRepository.java
@@ -1,17 +1,19 @@
 package com.noah.matdongsan.repository.user;
 
-import com.noah.matdongsan.entity.property.Property;
 import com.noah.matdongsan.entity.user.CommonUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 public interface CommonUserRepository extends JpaRepository<CommonUser,Long> {
     @Modifying
     @Query("UPDATE CommonUser u SET u.isRemoved = true WHERE u.id = :id")
     int softDeleteById(@Param("id") Long id);
+
+    Optional<CommonUser> findByEmail(String email);
 }
