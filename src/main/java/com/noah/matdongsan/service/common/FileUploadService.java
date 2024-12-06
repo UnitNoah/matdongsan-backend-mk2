c8ccd3e7eed6d1d2ed08e676f3b2e6064c417363
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-12-06 19:10:18 +0900

    feat: file upload 서비스 클래스 추가

---
+++ src/main/java/com/noah/matdongsan/service/common/FileUploadService.java
@@ -0,0 +1,31 @@
+package com.noah.matdongsan.service.common;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class FileUploadService {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+
+        amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
+        return "https://" + bucket + "/" + fileName;
+    }
+}
+
