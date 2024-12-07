2a06cb967ef09f30f9d6b5fec4b02fad6fa3310f
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-12-07 20:29:15 +0900

    refactor(entity): 가격 필드 추가, 유저 외래키 관게추가, 생성자 추가

--- src/main/java/com/noah/matdongsan/entity/user/Ticket.java
+++ src/main/java/com/noah/matdongsan/entity/user/Ticket.java
@@ -1,11 +1,14 @@
 package com.noah.matdongsan.entity.user;
 
 import com.noah.matdongsan.entity.BaseTimeEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Ticket extends BaseTimeEntity {
     @Id
     @Column(name = "ticket_id")
@@ -15,7 +18,16 @@ public class Ticket extends BaseTimeEntity {
     @Column(nullable = false)
     private Integer amount;
 
+    @Column(nullable = false)
+    private Integer price;
 
+    @OneToOne
+    @JoinColumn(name="common_user_id")
+    private CommonUser commonUser;
 
-
+    public Ticket(Integer amount, Integer price, CommonUser commonUser) {
+        this.amount = amount;
+        this.price = price;
+        this.commonUser = commonUser;
+    }
 }
