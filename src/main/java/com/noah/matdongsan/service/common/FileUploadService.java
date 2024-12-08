c961100630c9491fa958e8dc7dfeb11b4e6c9c38
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-12-08 18:23:12 +0900

    refactor(service): s3 업로드 부분 트렌젝션 어노테이션 추가

--- src/main/java/com/noah/matdongsan/service/common/FileUploadService.java
+++ src/main/java/com/noah/matdongsan/service/common/FileUploadService.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -17,7 +18,7 @@ public class FileUploadService {
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
-
+    @Transactional
     public String uploadFile(MultipartFile file) throws IOException {
         String fileName = file.getOriginalFilename();
         ObjectMetadata metadata = new ObjectMetadata();
