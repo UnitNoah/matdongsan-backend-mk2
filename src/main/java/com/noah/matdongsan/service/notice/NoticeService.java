ffde402de07498bc8f9add01b1408209a98b65f7
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-12-08 18:27:25 +0900

    feat(service): notice 관련 getNoticeList 추가

--- src/main/java/com/noah/matdongsan/service/notice/NoticeService.java
+++ src/main/java/com/noah/matdongsan/service/notice/NoticeService.java
@@ -1,7 +1,19 @@
 package com.noah.matdongsan.service.notice;
 
+import com.noah.matdongsan.dto.notice.NoticeReadDto;
+import com.noah.matdongsan.repository.notice.NoticeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class NoticeService {
+
+    private final NoticeRepository noticeRepository;
+
+    public List<NoticeReadDto> getNoticeList() {
+        return noticeRepository.getNotices();
+    }
 }
