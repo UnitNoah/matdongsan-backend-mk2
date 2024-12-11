f5c11e7e6c0ee1e58ec24d8fc75e50b6663666a0
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-12-11 15:51:28 +0900

    feat(Controller): 메소드 추가
    - 공지사항 생성 메소드
    - 1:1 문의 생성 메소드

--- src/main/java/com/noah/matdongsan/controller/NoticeController.java
+++ src/main/java/com/noah/matdongsan/controller/NoticeController.java
@@ -1,15 +1,16 @@
 package com.noah.matdongsan.controller;
 
-import com.noah.matdongsan.dto.notice.NoticeReadDto;
-import com.noah.matdongsan.dto.notice.PagedResponseDto;
+import com.amazonaws.Response;
+import com.noah.matdongsan.dto.notice.*;
 import com.noah.matdongsan.service.notice.NoticeService;
+import com.noah.matdongsan.service.notice.QuestionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -19,12 +20,13 @@ public class NoticeController {
 
     private final NoticeService noticeService;
 
+    private final QuestionService questionService;
+
     @GetMapping
     public ResponseEntity<PagedResponseDto<NoticeReadDto>> getNoticesList(
             @RequestParam(name = "pageNo", required = false, defaultValue = "1") int pageNo,
-            @RequestParam( name = "criteria" , required = false, defaultValue = "createdDate") String criteria,
-            @RequestParam( name = "keyword" , required = false, defaultValue = "") String keyword)
-    {
+            @RequestParam(name = "criteria", required = false, defaultValue = "createdDate") String criteria,
+            @RequestParam(name = "keyword", required = false, defaultValue = "") String keyword) {
 
         PagedResponseDto<NoticeReadDto> notices = noticeService.getNoticeList(pageNo, criteria, keyword);
         return ResponseEntity.ok(notices);
@@ -32,12 +34,27 @@ public class NoticeController {
 
     //admin만 작성하도록 해야함
     //공지사항 생성
-    //공지사항 디테일 읽기
+    @Secured("ROLE_MANAGER")
+    @PostMapping
+    public void createNotice(NoticeCreateDto noticeCreateDto) {
+        log.info(noticeCreateDto.toString());
+    }
+
+    @GetMapping("/detailNotice/{id}")
+    public ResponseEntity<NoticeDetailReadDto> getNoticeDetailData(@PathVariable(name = "id") Long id) {
+        NoticeDetailReadDto detail = noticeService.getDetailPageById(id);
+        return ResponseEntity.ok(detail);
+    }
+
     //공지사항 삭제
     //공지사항 수정
 
-    //이메일 받아야함
     //1:1 문의 생성
+    @PostMapping("/customerInquiry")
+    public void createCustomerInquiry(@ModelAttribute CustomerQuestionCreateDto dto) throws IOException {
+        questionService.createCustomerInquiry(dto);
+    }
+
     //1:1 문의 삭제
     //1:1 문의 수정
     //1:1 문의 읽기
