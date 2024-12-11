5c48dd523dbe2f72ee42f60c60a9333b82dcda2b
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-12-11 09:51:15 +0900

    refactor(Dto): NoticeReadDto부분 개선
    - 엔티티-> Dto로 데이터 변경하는 from 함수
    - createdAt 필드 및 날짜형식 추가

--- src/main/java/com/noah/matdongsan/dto/notice/NoticeReadDto.java
+++ src/main/java/com/noah/matdongsan/dto/notice/NoticeReadDto.java
@@ -1,9 +1,12 @@
 package com.noah.matdongsan.dto.notice;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.noah.matdongsan.entity.notice.Notice;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,9 +16,16 @@ public class NoticeReadDto {
 
     private String title;
 
-    public NoticeReadDto(long id, String title) {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDateTime createdAt;
+
+    public NoticeReadDto(long id, String title,LocalDateTime createdAt) {
         this.id = id;
         this.title = title;
+        this.createdAt = createdAt;
     }
 
+    public static NoticeReadDto from(Notice notice) {
+        return new NoticeReadDto(notice.getId(), notice.getTitle(),notice.getCreatedDate());
+    }
 }
