2d2de1e963e58ef0b770c86a9e70532c5886853f
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-12-11 09:52:31 +0900

    refactor(Controller): NoticeController부분 페이지네이션 오류 개선
    - 검색값, 정렬값 parameter 추가
    - 페이지네이션을 위한 리턴값 PagedResponseDto로 수정

--- src/main/java/com/noah/matdongsan/controller/NoticeController.java
+++ src/main/java/com/noah/matdongsan/controller/NoticeController.java
@@ -1,6 +1,7 @@
 package com.noah.matdongsan.controller;
 
 import com.noah.matdongsan.dto.notice.NoticeReadDto;
+import com.noah.matdongsan.dto.notice.PagedResponseDto;
 import com.noah.matdongsan.service.notice.NoticeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -21,12 +20,28 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     @GetMapping
-    public ResponseEntity<List<NoticeReadDto>> getNoticesList(
-            @RequestParam(name = "pageNo",required = false, defaultValue = "1") String pageNo
-    ) {
+    public ResponseEntity<PagedResponseDto<NoticeReadDto>> getNoticesList(
+            @RequestParam(name = "pageNo", required = false, defaultValue = "1") int pageNo,
+            @RequestParam( name = "criteria" , required = false, defaultValue = "createdDate") String criteria,
+            @RequestParam( name = "keyword" , required = false, defaultValue = "") String keyword)
+    {
 
-        // 파라미터를 서비스에 전달
-        List<NoticeReadDto> notices = noticeService.getNoticeList();
+        PagedResponseDto<NoticeReadDto> notices = noticeService.getNoticeList(pageNo, criteria, keyword);
         return ResponseEntity.ok(notices);
     }
+
+    //admin만 작성하도록 해야함
+    //공지사항 생성
+    //공지사항 디테일 읽기
+    //공지사항 삭제
+    //공지사항 수정
+
+    //이메일 받아야함
+    //1:1 문의 생성
+    //1:1 문의 삭제
+    //1:1 문의 수정
+    //1:1 문의 읽기
+    //1:1 문의리스트 읽기
+
+
 }
