56819e1cbdede3bb1a275743c99f7e8da5b78c19
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-12-21 23:00:28 +0900

    feat(Service): createProperty 메소드 생성

--- src/main/java/com/noah/matdongsan/service/property/PropertyService.java
+++ src/main/java/com/noah/matdongsan/service/property/PropertyService.java
@@ -1,33 +1,69 @@
 package com.noah.matdongsan.service.property;
 
 import com.noah.matdongsan.dto.property.PropertyCreateDto;
+import com.noah.matdongsan.entity.property.Property;
 import com.noah.matdongsan.entity.user.CommonUser;
 import com.noah.matdongsan.entity.user.Ticket;
 import com.noah.matdongsan.repository.property.PropertyRepository;
-import com.noah.matdongsan.repository.user.CommonUserRepository;
 import com.noah.matdongsan.repository.user.TicketRepository;
+import com.noah.matdongsan.service.common.FileUploadService;
+import com.noah.matdongsan.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class PropertyService {
 
     private final PropertyRepository propertyRepository;
     private final TicketRepository ticketRepository;
-    private final CommonUserRepository commonUserRepository;
+    private final MemberService memberService;
+    private final FileUploadService fileUploadService;
+    private final PropertyDetailService propertyDetailService;
+    private final PropertyPhotoService propertyPhotoService;
 
+    @Transactional
     public void updateTicketAmount(String email, int amount) {
-        CommonUser commonUser = commonUserRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("User not found with email: " + email));
+        CommonUser commonUser = memberService.findUserByEmail(email);
 
         int price = calculatePrice(amount);
 
         Ticket ticket = new Ticket(amount, price, commonUser);
         ticketRepository.save(ticket);
     }
+
+    //생성
+    @Transactional
+    public void createProperty(PropertyCreateDto dto,String email) {
+
+        CommonUser commonUser = memberService.findUserByEmail(email);
+
+        Property property = Property.builder()
+                .deposit(dto.getPdeposite())
+                .monthlyFee(dto.getPrentalfee())
+                .maintenance(dto.getPmaintenance())
+                .floorType(dto.getPfloortype())
+                .floor(dto.getPfloor())
+                .roomSize(dto.getPsize())
+                .title(dto.getPtitle())
+                .category(dto.getPcategory())
+                .address(dto.createAddress())
+                .commonUser(commonUser)
+                .build();
+
+        if (dto.getPthumbnail() != null && !dto.getPthumbnail().isEmpty()) {
+            String thumbnailUrl = fileUploadService.uploadFile(dto.getPthumbnail());
+            property.updateThumbnailUrl(thumbnailUrl);
+        }
+
+        propertyRepository.save(property);
+        propertyDetailService.create(dto.getPropertyDetail(),property);
+        propertyPhotoService.create(dto.getPpattach(),property);
+    }
+
 
     private int calculatePrice(int amount) {
         return switch (amount) {
@@ -36,11 +72,5 @@ public class PropertyService {
             case 5 -> 25000;
             default -> throw new IllegalArgumentException("Invalid amount: " + amount);
         };
-    }
-
-    //생성
-    public void createProperty(PropertyCreateDto propertyCreateDto) {
-        //가져온 dto 데이터를 엔티티로 분리 해야함
-        //propertyCreateDto
     }
 }
