36b42d8b82ebe0016d425f4d852fc6790d27cb32
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-12-21 22:52:24 +0900

    refactor(Controller): 로그인 중인 유저 데이터 관련 코드 추가

--- src/main/java/com/noah/matdongsan/controller/PropertyController.java
+++ src/main/java/com/noah/matdongsan/controller/PropertyController.java
@@ -1,11 +1,16 @@
 package com.noah.matdongsan.controller;
 
 import com.noah.matdongsan.dto.property.PropertyCreateDto;
+import com.noah.matdongsan.dto.property.PropertyReadDto;
 import com.noah.matdongsan.service.property.PropertyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -18,12 +23,19 @@ public class PropertyController {
     @PostMapping("/payment/ticket/{product}")
     public void purchaseTicket(@PathVariable(name="product") int product){
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        log.info("상품 " +product);
         propertyService.updateTicketAmount(email,product);
     }
 
     @PostMapping("/registerForm")
-    public void createProperty(@ModelAttribute PropertyCreateDto propertyCreateDto){
-        log.info(propertyCreateDto.toString());
+    public void createProperty(@ModelAttribute PropertyCreateDto propertyCreateDto,@AuthenticationPrincipal String userEmail) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        propertyService.createProperty(propertyCreateDto,email);
+    }
+
+    //페이지# 받아서 인피니티 스크롤 구현 해야함
+    @GetMapping
+    public ResponseEntity<List<PropertyReadDto>> getProperties(){
+        
+        return null;
     }
 }
